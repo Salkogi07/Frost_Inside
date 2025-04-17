2490c8b19098939808abc884900379f867adfd4c
junhek2525 <135084879+junhek2525@users.noreply.github.com>
2025-04-17 22:19:57 +0900

    16.7
    16.7

---
+++ Assets/Script/monster/MimicBox/Monster_stat.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Monster_stat : MonoBehaviour
+{
+
+    [Header("¿œπ› Ω∫≈»")]
+    [SerializeField] public float Health;
+    [SerializeField] public float MaxHealth;
+    [SerializeField] public float Damage;
+    [SerializeField] public float Armor;
+    [SerializeField] public float Groggy = 0f;
+    [SerializeField] public float speed = 1f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
