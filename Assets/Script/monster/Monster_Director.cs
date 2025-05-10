f32574696984e9a201dc02bc16372676eec4df40
junhek2525 <135084879+junhek2525@users.noreply.github.com>
2025-05-10 22:13:23 +0900

    19
    19

--- Assets/Script/monster/Monster_Director.cs
+++ Assets/Script/monster/Monster_Director.cs
@@ -1,17 +1,18 @@
-using System.Collections;
+ using System.Collections;
 using UnityEngine;
-
 using System.Collections.Generic;
 //using System;
 using UnityEngine.EventSystems;
 using UnityEditor;
-
-
-public class MimicBoxDirector : MonoBehaviour
+public class Monster_Director : MonoBehaviour
 {
-    [Header ("Mimic stats")]
-    [SerializeField]  public float detectionRadius = 1f; // 적이 플레이어를 감지할 범위
-    [SerializeField]  private Transform Player; // 플레이어의 위치
+
+
+
+
+    [Header("Mimic stats")]
+    [SerializeField] public float detectionRadius = 1f; // 적이 플레이어를 감지할 범위
+    [SerializeField] private Transform Player; // 플레이어의 위치
     [SerializeField] public float jumpspeed = 5f;
     [SerializeField] public float jumpcooltime;
     [SerializeField] public float jumpingmax;
@@ -25,7 +26,7 @@ public class MimicBoxDirector : MonoBehaviour
     bool Scoping = false;
     int moverandomDirection; //랜덤좌우방향으로 이동하는 변수
     float Concealment_time;
-    bool Moving =  false;
+    bool Moving = false;
     float Moving_Time;
     float distance;
     bool jumping = false;
@@ -74,41 +75,41 @@ public class MimicBoxDirector : MonoBehaviour
         //Player = GameObject.FindWithTag("Player").transform;
 
         Monster_Jump = GetComponent<Monster_Jump>();
-            Debug.Log("해냇따");
-        
+        Debug.Log("해냇따");
+
     }
     void Start()
     {
-        
+
         //attack = attack.GetComponent<Mimic_attack>();
-        Player = GameObject.FindWithTag("Player").transform; 
+        Player = GameObject.FindWithTag("Player").transform;
 
         //if(Monster_Jump != null)
         //{
         //Monster_Jump.OnJump();
         //}
-        
-    }
-    
 
-    
+    }
+
+
+
 
     // 범위 밖으로 나가면 호출
-    
+
 
     // 플레이어를 추적하는 함수
 
 
     // 매 프레임마다 플레이어와의 거리 체크
     private void Update()
-    {   
-        
+    {
 
-        if(Scoping == false && Pattern != pattern.Concealment)
+
+        if (Scoping == false && Pattern != pattern.Concealment)
         {
-            
-            Concealment_time  += Time.deltaTime;
-            if(Concealment_time >= 30f)
+
+            Concealment_time += Time.deltaTime;
+            if (Concealment_time >= 30f)
             {
                 Pattern = pattern.Concealment;
             }
@@ -119,9 +120,9 @@ public class MimicBoxDirector : MonoBehaviour
         }
         switch (Pattern)
         {
-         
+
             case pattern.Concealment:
-                if(Scoping == false)
+                if (Scoping == false)
                 {
                     //바로 숨어듬
                     Hide = true;
@@ -140,43 +141,43 @@ public class MimicBoxDirector : MonoBehaviour
             case pattern.exiting:
                 //나오는 애니메이션
 
-                Pattern =  pattern.Move;
+                Pattern = pattern.Move;
                 Scoping = false;
                 break;
             case pattern.Move:
 
                 /*Debug.Log("ddeee");*/
 
-                
-                    Move();
-                
+
+                Move();
+
                 if (Moving == false && Moving_Time >= 5f)
-                
-                    
+
+
                 {
                     moverandomDirection = Random.Range(1, 3) == 1 ? 1 : -1;
-                    
-                    
+
+
                     Moving_Time = 0f;
                     Moving = true;
                     distance = 0f;
                 }
-                else if(Moving == false)
+                else if (Moving == false)
                 {
                     Moving_Time += Time.deltaTime;
                 }
-                
+
                 //Debug.Log("움직인다!");
 
-                    // 수정 필요 (5초후 방향을 정하고 일정거리 이동필요)
+                // 수정 필요 (5초후 방향을 정하고 일정거리 이동필요)
 
                 break;
 
             case pattern.Chase:
-                
+
                 Chase();
                 direction();
-                Moving_Time = 0f;
+
 
 
                 break;
@@ -192,22 +193,22 @@ public class MimicBoxDirector : MonoBehaviour
 
 
         }
-        
-    } 
+
+    }
     private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !Hide) // 플레이어 태그를 가진 객체 감지
         {
-            if (other.CompareTag("Player") && !Hide ) // 플레이어 태그를 가진 객체 감지
-            {
-            
-                Pattern = pattern.Chase;
-                Scoping = true;
-                Moving = false;
+
+            Pattern = pattern.Chase;
+            Scoping = true;
+            Moving = false;
         }
 
-        }
+    }
     private void OnTriggerStay2D(Collider2D other)
     {
-        if(other.CompareTag("Player") && !Hide)
+        if (other.CompareTag("Player") && !Hide)
         {
             yDistance = Player.position.y - transform.position.y;
             if (yDistance > 1f && Monster_Jump.jump_cooltime <= 0f)
@@ -229,44 +230,46 @@ public class MimicBoxDirector : MonoBehaviour
     }
     private void Move()
     {
-        
+
         if (Moving)
-        { if(distance < distanceMax)
+        {
+            if (distance < distanceMax)
             {
-                
-                if(FloorMeasurement.Groundcheck)
+
+                if (FloorMeasurement.Groundcheck)
                 {
                     transform.position += new Vector3(moverandomDirection * stat.speed * Time.deltaTime, 0f, 0f);
-                distance += Time.deltaTime;
-                    hillDetections.CheckForHillAhead();
+                    distance += Time.deltaTime;
+
                 }
-                else { 
+                else
+                {
                     moverandomDirection = -moverandomDirection;
 
                 }
-                   direction(); 
-                
+                direction();
+
             }
-            else 
+            else
             {
                 Moving = false;
 
             }
         }
-        
 
 
-        
 
 
-        
+
+
+
     }
 
     private void direction()
     {
         if (Pattern == pattern.Move)
         {
-            moveDirection = moverandomDirection == 1 ? 1f : -1f ;
+            moveDirection = moverandomDirection == 1 ? 1f : -1f;
         }
 
 
@@ -278,39 +281,46 @@ public class MimicBoxDirector : MonoBehaviour
         {
             Floor_Measurement.position = new Vector3(transform.position.x - 1f, Floor_Measurement.position.y, 0f);
             attack.position = new Vector3(transform.position.x - stat.range[0], attack.position.y, 0f);
-            HillDetection.position = new Vector3(transform.position.x - (hillDetections.boxSize[0]* 3f),HillDetection.position.y, 0f);
+            HillDetection.position = new Vector3(transform.position.x - (hillDetections.boxSize[0] * 3f), HillDetection.position.y, 0f);
         }
         else
         {
             Floor_Measurement.position = new Vector3(transform.position.x + 1f, Floor_Measurement.position.y, 0f);
             attack.position = new Vector3(transform.position.x + stat.range[0], attack.position.y, 0f);
-            HillDetection.position = new Vector3(transform.position.x+ (hillDetections.boxSize[0] *3f), HillDetection.position.y, 0f);
+            HillDetection.position = new Vector3(transform.position.x + (hillDetections.boxSize[0] * 3f), HillDetection.position.y, 0f);
         }
-       
-    
+
+
 
 
     }
     private void jump()
     {
-        
+
         jumping = true;
-        if(jumpingmax >= 1f)
+        if (jumpingmax >= 1f)
         {
             transform.position += new Vector3(0f, jumpspeed, 0f);
             jumpingmax += Time.deltaTime;
         }
-        
+
     }
     private void Chase()
     {
-        
+
         //FloorMeasurement.direction = moveDirection;
-
         transform.position += new Vector3(moveDirection * stat.speed * Time.deltaTime, 0f, 0f);
-        hillDetections.CheckForHillAhead();
 
+        if (hillDetections.Groundcheck == true)
+        {
+            //transform.position += new Vector3(0f, 0.1f, 0f);
+        }
+        else
+        {
 
+        }
+
+        Vector3 newFloorPosition = Floor_Measurement.position;
 
 
 
@@ -320,3 +330,4 @@ public class MimicBoxDirector : MonoBehaviour
 
     }
 }
+
