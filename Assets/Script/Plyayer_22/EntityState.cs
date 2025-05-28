480389cd1342028a8bd1219a1a11282677ccd0f7
kogisal <kogisal099@gmail.com>
2025-05-28 21:42:24 +0900

    player_Update

--- Assets/Script/Plyayer_22/EntityState.cs
+++ Assets/Script/Plyayer_22/EntityState.cs
@@ -4,27 +4,35 @@ public abstract class EntityState
 {
     protected Player player;
     protected StateMachine stateMachine;
-    protected string stateName;
+    protected string animBoolName;
 
-    public EntityState(Player player, StateMachine stateMachine, string stateName)
+    protected Animator anim;
+    protected Rigidbody2D rb;
+    protected PlayerInputSet input;
+
+    public EntityState(Player player, StateMachine stateMachine, string animBoolName)
     {
         this.player = player;
         this.stateMachine = stateMachine;
-        this.stateName = stateName;
+        this.animBoolName = animBoolName;
+
+        anim = player.anim;
+        rb = player.rb;
+        input = player.input;
     }
 
     public virtual void Enter()
     {
-        Debug.Log("I enter" + stateName);
+        anim.SetBool(animBoolName, true);
     }
 
     public virtual void Update()
     {
-        Debug.Log("I update" + stateName);
+        anim.SetFloat("yVelocity", rb.linearVelocity.y);
     }
 
     public virtual void Exit()
     {
-        Debug.Log("I exit" + stateName);
+        anim.SetBool(animBoolName, false);
     }
 }
--- Assets/Script/Plyayer_22/Player_IdleState.cs
+++ Assets/Script/Plyayer_22/Player_IdleState.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
 
-public class Player_IdleState : EntityState
+public class Player_IdleState : Player_GroundedState
 {
     public Player_IdleState(Player player, StateMachine stateMachine, string stateName) : base(player, stateMachine, stateName)
     {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+
+        player.SetVelocity(0, rb.linearVelocity.y);
     }
 
     public override void Update()
@@ -12,5 +19,8 @@ public class Player_IdleState : EntityState
 
         if (player.moveInput.x != 0)
             stateMachine.ChangeState(player.moveState);
+
+        if (player.input.Player.Jump.WasPressedThisFrame())
+            stateMachine.ChangeState(player.jumpState);
     }
 }
