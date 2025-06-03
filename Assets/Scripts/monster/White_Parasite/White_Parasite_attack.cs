18fd3b78dcf249d84dc2e77d4f8975f62d90f8a2
ddddsssswwww <junhek2525@gmail.com>
2025-06-03 23:55:18 +0900

    [Add]지하유령코드
    지하유령

---
+++ Assets/Scripts/monster/White_Parasite/White_Parasite_attack.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class White_Parasite_attack : MonoBehaviour
+{
+    public Vector2 Boxsize = new Vector2(1f,1f);
+    public LayerMask groundLayer;
+    
+
+    private Monster_stat stat;
+    private White_Parasite_Director white_Parasite_Director;
+      
+    void Start()
+    {
+        stat = GetComponent<Monster_stat>();
+        white_Parasite_Director = GetComponent<White_Parasite_Director>();
+        // Boxsize = new Vector2(stat.range.x, stat.range.y);
+        Boxsize = new Vector2(stat.range[0], stat.range[1]);
+    }
+    
+    void Update()
+    {
+        Collider2D[] hit = Physics2D.OverlapBoxAll(transform.position, Boxsize,0f);
+        foreach(Collider2D collider in hit)
+        {
+            if(collider.tag == "Player" )
+            {
+                white_Parasite_Director.Pattern = White_Parasite_Director.pattern.Attack;
+                //인벤토리에 들어가 티타임
+                //도와주시오
+                Debug.Log("티타임");
+            }
+        }
+    }
+    
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, Boxsize);
+    }
+}
