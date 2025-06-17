1366fa0e39b4f18b7fca1186fda7cf0c1dd0a97f
ddddsssswwww <junhek2525@gmail.com>
2025-06-17 23:11:52 +0900

    [Add] Tester

---
+++ Assets/Scripts/monster/the tester/Tester.cs
@@ -0,0 +1,17 @@
+using Scripts.Enemy;
+using UnityEngine;
+
+public class Tester : Enemy
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+            
+        EnemyStateMachine.Initialize(IdleState);
+    }
+}
