2028def3019ff4b07fb483e9ebecaf8cc14b8a29
Salkogi07 <kogisal099@gmail.com>
2025-08-22 13:08:56 +0900

    crtl + alt + enter

--- Assets/Scripts/Entity/Enemy/EnemyState.cs
+++ Assets/Scripts/Entity/Enemy/EnemyState.cs
@@ -1,59 +1,58 @@
 ﻿using UnityEngine;
 
+public abstract class EnemyState
+{
+    protected Enemy enemy;
+    protected Enemy_StateMachine enemyStateMachine;
+    protected string animBoolName;
 
-    public abstract class EnemyState
+    protected Animator anim;
+    protected Rigidbody2D rigidbody;
+
+    protected float stateTimer;
+    protected bool triggerCalled;
+
+    public EnemyState(Enemy enemy, Enemy_StateMachine enemyStateMachine, string animBoolName)
     {
-        protected Enemy enemy;
-        protected Enemy_StateMachine enemyStateMachine;
-        protected string animBoolName;
+        this.enemy = enemy;
+        this.enemyStateMachine = enemyStateMachine;
+        this.animBoolName = animBoolName;
 
-        protected Animator anim;
-        protected Rigidbody2D rigidbody;
-        
-        protected float stateTimer;
-        protected bool triggerCalled;
-
-        public EnemyState(Enemy enemy, Enemy_StateMachine enemyStateMachine, string animBoolName)
-        {
-            this.enemy = enemy;
-            this.enemyStateMachine = enemyStateMachine;
-            this.animBoolName = animBoolName;
-            
-            anim = enemy.Anim;
-            rigidbody = enemy.rb;
-        }
-
-        public virtual void Enter()
-        {
-            anim.SetBool(animBoolName, true);
-            triggerCalled = false;
-        }
-
-        public virtual void Update()
-        {
-            stateTimer -= Time.deltaTime;
-
-			float battleAnimSpeedMultiplier = enemy.battleMoveSpeed / enemy.MoveSpeed;
-
-            anim.SetFloat("BattleAnimSpeedMultiplier",battleAnimSpeedMultiplier);
-            anim.SetFloat("moveAnimSpeedMultiplier",enemy.moveAnimSpeedMultiplier);
-             // anim.SetFloat("xVelocity", rigidbody.linearVelocity.x);
-            anim.SetFloat("xVelocity", Mathf.Clamp(rigidbody.linearVelocityX, -1f, 1f));
-        }
-
-        public virtual void FiexedUpdate()
-        {
-            anim.SetFloat("moveAnimSpeedMultiplier",enemy.moveAnimSpeedMultiplier);
-            // anim.SetFloat("yVelocity", rigidbody.linearVelocity.y);
-        }
-
-        public virtual void Exit()
-        {
-            anim.SetBool(animBoolName, false);
-        }
-
-        public void CallAnimationTrigger()
-        {
-            triggerCalled = true;
-        }
+        anim = enemy.Anim;
+        rigidbody = enemy.rb;
     }
+
+    public virtual void Enter()
+    {
+        anim.SetBool(animBoolName, true);
+        triggerCalled = false;
+    }
+
+    public virtual void Update()
+    {
+        stateTimer -= Time.deltaTime;
+
+        float battleAnimSpeedMultiplier = enemy.battleMoveSpeed / enemy.MoveSpeed;
+
+        anim.SetFloat("BattleAnimSpeedMultiplier", battleAnimSpeedMultiplier);
+        anim.SetFloat("moveAnimSpeedMultiplier", enemy.moveAnimSpeedMultiplier);
+        // anim.SetFloat("xVelocity", rigidbody.linearVelocity.x);
+        anim.SetFloat("xVelocity", Mathf.Clamp(rigidbody.linearVelocityX, -1f, 1f));
+    }
+
+    public virtual void FiexedUpdate()
+    {
+        anim.SetFloat("moveAnimSpeedMultiplier", enemy.moveAnimSpeedMultiplier);
+        // anim.SetFloat("yVelocity", rigidbody.linearVelocity.y);
+    }
+
+    public virtual void Exit()
+    {
+        anim.SetBool(animBoolName, false);
+    }
+
+    public void CallAnimationTrigger()
+    {
+        triggerCalled = true;
+    }
+}
