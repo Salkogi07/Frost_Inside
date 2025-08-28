52dc8530428b8bb59b9a944f34d1627e58e8a225
ddddsssswwww <junhek2525@gmail.com>
2025-08-28 20:36:38 +0900

    [fix] bug close

--- Assets/Scripts/Entity/Enemy/Arctic_fox/Enemy_ArcticFox.cs
+++ Assets/Scripts/Entity/Enemy/Arctic_fox/Enemy_ArcticFox.cs
@@ -1,26 +1,26 @@
 ﻿
-    public class Enemy_ArcticFox : Enemy
-    {
-        
-        public Enemy_IdleState IdleState { get; private set; }
-        public Enemy_MoveState MoveState { get; private set; }
-        public Enemy_AttackState AttackState { get; private set; }
-        public Enemy_ChaseState  ChaseState { get; private set; }
-        public Enemy_DeadState  DeadState { get; private set; }
-        
-        protected virtual void Awake()
-        {
-            base.Awake();
-            IdleState = new Enemy_IdleState(this, EnemyStateMachine, "idle");
-            MoveState = new Enemy_MoveState(this, EnemyStateMachine, "move");
-            AttackState = new Enemy_AttackState(this, EnemyStateMachine, "attack");
-            ChaseState = new Enemy_ChaseState(this, EnemyStateMachine, "Chase");
-            DeadState = new Enemy_DeadState(this, EnemyStateMachine, "dead");
-            
-        }
-
-        protected override void Start()
-        {
-            base.Start();
-        }
-    }
+    // public class Enemy_ArcticFox : Enemy
+    // {
+    //     
+    //     public Enemy_IdleState IdleState { get; private set; }
+    //     public Enemy_MoveState MoveState { get; private set; }
+    //     public Enemy_AttackState AttackState { get; private set; }
+    //     public Enemy_ChaseState  ChaseState { get; private set; }
+    //     public Enemy_DeadState  DeadState { get; private set; }
+    //     
+    //     protected virtual void Awake()
+    //     {
+    //         base.Awake();
+    //         IdleState = new Enemy_IdleState(this, EnemyStateMachine, "idle");
+    //         MoveState = new Enemy_MoveState(this, EnemyStateMachine, "move");
+    //         AttackState = new Enemy_AttackState(this, EnemyStateMachine, "attack");
+    //         ChaseState = new Enemy_ChaseState(this, EnemyStateMachine, "Chase");
+    //         DeadState = new Enemy_DeadState(this, EnemyStateMachine, "dead");
+    //         
+    //     }
+    //
+    //     protected override void Start()
+    //     {
+    //         base.Start();
+    //     }
+    // }
--- Assets/Scripts/monster/the tester/Tester.cs
+++ Assets/Scripts/monster/the tester/Tester.cs
@@ -3,23 +3,23 @@
 
     public class Tester : Enemy
     {
-        public Enemy_IdleState IdleState { get; private set; }
-        public Enemy_MoveState MoveState { get; private set; }
-        public Enemy_ChaseState  ChaseState { get; private set; }
-        
-        protected override void Awake()
-        {
-            base.Awake();
-            IdleState = new Enemy_IdleState(this, EnemyStateMachine, "idle");
-            MoveState = new Enemy_MoveState(this, EnemyStateMachine, "move");
-            ChaseState = new Enemy_ChaseState(this, EnemyStateMachine, "Chase");
-        }
-
-        protected override void Start()
-        {
-            base.Start();
-
-            EnemyStateMachine.Initialize(IdleState);
-        }
+        // public Enemy_IdleState IdleState { get; private set; }
+        // public Enemy_MoveState MoveState { get; private set; }
+        // public Enemy_ChaseState  ChaseState { get; private set; }
+        //
+        // protected override void Awake()
+        // {
+        //     base.Awake();
+        //     IdleState = new Enemy_IdleState(this, EnemyStateMachine, "idle");
+        //     MoveState = new Enemy_MoveState(this, EnemyStateMachine, "move");
+        //     ChaseState = new Enemy_ChaseState(this, EnemyStateMachine, "Chase");
+        // }
+        //
+        // protected override void Start()
+        // {
+        //     base.Start();
+        //
+        //     EnemyStateMachine.Initialize(IdleState);
+        // }
     }
 
