091cd2f93f6972e50dd1644ba0ea1bb9a4244ac9
ddddsssswwww <junhek2525@gmail.com>
2025-09-03 00:19:37 +0900

    [Update] jumpstate completion

--- Assets/Scripts/Stats/Enemy_Stats.cs
+++ Assets/Scripts/Stats/Enemy_Stats.cs
@@ -4,28 +4,29 @@ using UnityEngine;
 public class Enemy_Stats : MonoBehaviour
 {
     [Header("Hp info")]
-    [SerializeField] public float hp = 0;
     [SerializeField] public float maxHp = 100;
 
+    [Header("Hp info")]
     [SerializeField] private int damage;
     [SerializeField] private int armor;
+    [SerializeField] public float Groggy;
     public int difficulty = 1;
 
     private void Start()
     {
-        hp = maxHp;
+        
     }
 
-    public virtual void TakeDamage(int _damage)
-    {
-        hp -= _damage;
-
-        if (hp < 0)
-            Die();
-    }
+    // public virtual void TakeDamage(int _damage)
+    // {
+    //     hp -= _damage;
+    //
+    //     if (hp < 0)
+    //         Die();
+    // }
 
     protected virtual void Die()
     {
-        throw new NotImplementedException();
+        
     }
 }
