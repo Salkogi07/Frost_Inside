7a704c87188418dcd6679aeb460d7f17b369603e
Salkogi07 <Salkogi07@users.noreply.github.com>
2025-09-16 01:46:45 +0900

    [Update] Monkey boom

--- Assets/Scripts/monster/Bomb Monkey/explosion_damage.cs
+++ Assets/Scripts/monster/Bomb Monkey/explosion_damage.cs
@@ -1,15 +1,34 @@
 using UnityEngine;
 using System.Collections;
 using Stats;
+using Unity.Netcode;
 
-public class explosion_damage : MonoBehaviour
+public class explosion_damage : NetworkBehaviour
 {
     public int damage;
     
-    private void OnTriggerEnter2D(Collider2D other)
+    public override void OnNetworkSpawn()
     {
-        other.GetComponent<Entity_Health>().TakeDamage(damage, transform);
-        Debug.Log(damage);
-        Destroy(gameObject,.5f);
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 3);
+        foreach (var collider in colliders)
+        {
+            if (collider.CompareTag("Player"))
+            {
+                if(collider.TryGetComponent<Entity_Health>(out Entity_Health health))
+                {
+                    health.TakeDamage(damage, gameObject.transform);
+                }
+            }
+        }
+        
+        Invoke(nameof(DestroySelf), 0.5f);
+    }
+    
+    private void DestroySelf()
+    {
+        if (IsServer)
+        {
+            NetworkObject.Despawn();
+        }
     }
 }
